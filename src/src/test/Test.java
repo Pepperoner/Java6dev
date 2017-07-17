4d0671ded87533824102c561fda6164469c17516
Pepperoner <boris.lahutin@gmail.com>
2017-07-17 18:32:39 +0300

    Test

--- src/src/test/Test.java
+++ src/src/test/Test.java
@@ -1,7 +1,8 @@
 package test;
 
-/**
- * Created by Pepperoner on 17.07.2017.
- */
 public class Test {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
 }
