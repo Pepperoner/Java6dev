cd08c538121e2ce9c6ed04e8a91ed4bab53c9f07
Pepperoner <boris.lahutin@gmail.com>
2017-07-18 16:42:39 +0300

    Test

--- src/Test.java
+++ src/Test.java
@@ -2,4 +2,7 @@
  * Created by Pepperoner on 17.07.2017.
  */
 public class Test {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+    }
 }
---
+++ src/module1/homework/Main.java
@@ -0,0 +1,7 @@
+package module1.homework;
+
+/**
+ * Created by Pepperoner on 18.07.2017.
+ */
+public class Main {
+}
