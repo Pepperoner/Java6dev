3cda1a5bf442b83f757281e540672f0130931d27
Pepperoner <boris.lahutin@gmail.com>
2017-07-25 19:07:38 +0300

    Delete 6.sql

--- src/module1/homework/6.sql
+++
@@ -1,3 +0,0 @@
-use dev_db;
-
-select avg(salary) from dev_db.developers where developers_id in(select developers_id from dev_db.projects_has_developers where projects_id = (select projects_id from dev_db.projects where cost = (select min(cost) from projects))); 
